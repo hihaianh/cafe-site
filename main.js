0c8dfc066f3e8096fe19f91ec068f4d1a20b7472
Anh <97702902+hihaianh@users.noreply.github.com>
2023-07-09 23:32:46 +0100

    added nav bar to every page
    using innerHTML - no other complicated methods. Alternative ways are commented out in main.js

--- main.js
+++ main.js
@@ -1,1 +1,87 @@
-//dropdown menu: https://www.youtube.com/watch?v=S-VeYcOCFZw
+// const newArr = [{
+//     element: 'li',
+//     children: [{
+//         element: 'a',
+//         innerText: 'hello',
+//         href: 'contact.html'
+//     }]
+// }, {
+//     element: 'li',
+//     children: [{
+//         element: 'a',
+//         innerText: 'about us',
+//         href: 'about.html'
+//     }]
+// }, {
+//     element: 'li',
+//     children: [{
+//         element: 'a',
+//         innerText: 'home',
+//         href: 'index.html'
+//     }]
+// }, {
+//     element: 'li',
+//     children: [{
+//         element: 'a',
+//         innerText: 'shop',
+//         href: 'shop.html'
+//     }]
+// }, {
+//     element: 'div',
+//     children: [{
+//         element: 'p',
+//         innerText: 'klev',
+//     }]    
+// }]
+
+// const header = document.getElementById('header')
+//       const nav = document.createElement('nav')
+//       header.appendChild(nav)
+//       const ul = document.createElement('ul')
+//       nav.appendChild(ul)
+
+//       newArr.forEach(item => {
+//         const e = document.createElement(item.element)
+//         if (item?.children) {
+//             const child = document.createElement(item.children[0].element)
+//             child.innerText = item.children[0]?.innerText
+//             child.setAttribute('href', item.children[0]?.href)
+//             e.appendChild(child)
+//         }
+//         ul.appendChild(e)
+//     })
+    //   const li = document.createElement('li')
+    //   ul.appendChild(li)
+    //   const a = document.createElement('a')
+    //   li.appendChild(a)
+    //   a.innerText = 'hello'
+    //   a.setAttribute('href', 'contact.html')
+
+    //   const liTwo = document.createElement('li')
+    //   ul.appendChild(liTwo)
+    //   const aTwo = document.createElement('a')
+    //   liTwo.appendChild(aTwo)
+    //   aTwo.innerText = 'about us'
+    //   aTwo.setAttribute('href', 'about.html')
+
+
+    header.innerHTML = `<nav>
+    <div style="width: 100px">
+      <img src="logo.png" style="width: 100%; height: auto" />
+    </div>
+    <ul>
+      <div class="dropdown">
+        <li><a href="#">Visit &#x2304;</a>
+          <div class="dropdown-content">
+              <a href="#">Shoreditch</a>
+              <a href="#">Brick Lane</a>
+              <a href="#">Menu</a>
+          </div>
+        </li>
+      </div>
+      <li><a href="#">Shop</a></li>
+      <li><a href="about.html">About Us</a></li>
+      <li><a href="contact.html">Contact</a></li>
+      <li><a href="#"><i class="fa-light fa-magnifying-glass"></i></a></li>
+    </ul>
+  </nav>`
