b8cae281ddb6367bdbf89e370a0485ac6e785185
Anh <97702902+hihaianh@users.noreply.github.com>
2023-07-13 22:14:40 +0100

    adjustments
    - include footer in all pages
    - added carousel buttons
    - added font awesome link to all pages
    - added header to shop page
    - styling changes to carousel and form in contact page (not home page footer form)

--- main.js
+++ main.js
@@ -67,7 +67,7 @@
 
     header.innerHTML = `<nav>
     <div style="width: 100px">
-      <img src="logo.png" style="width: 100%; height: auto" />
+      <a href="index.html"><img src="logo.png" style="width: 100%; height: auto" /></a>
     </div>
     <ul>
       <div class="dropdown">
@@ -82,8 +82,75 @@
       <li><a href="#">Shop</a></li>
       <li><a href="about.html">About Us</a></li>
       <li><a href="contact.html">Contact</a></li>
-      <li><a href="#"><i class="fa-light fa-magnifying-glass"></i></a></li>
+      <li>
+        <a href="#">
+          <i class="fa-light fa-magnifying-glass"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa-sharp fa-light fa-cart-shopping" style="color: #000000;"></i>
+        </a>
+      </li>
     </ul>
   </nav>`
 
-    footer.innerHTML = ``
+    footer.innerHTML = `<div id="footer-container">
+    <div class="columns">
+      <div class="column">
+        <img src="./images/logo-brown.png" alt="logo">
+        <div class="sns-icons">
+          <ul>
+            <li>
+              <a href="#">
+                <i class="fa-brands fa-instagram fa-xl" style="color: #000000;"></i>
+              </a>
+              <li>
+                <a href="#">
+                  <i class="fa-brands fa-square-facebook fa-xl" style="color: #000000;"></i>
+                </a>
+              </li>
+              
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- <div class="column">
+        <p>99 Hackney Road, Shoreditch, London, E1 8TB</p>
+        <p>14 Brick Lane, London, E2 9DF</p>
+      </div> -->
+
+      <div class="column">
+        <ul>
+          <li><a href="shop.html">Shop</a></li>
+          <li><a href="menu.html">Menu</a></li>
+          <li><a href="about.html">About Us</a></li>
+          <li><a href="contact.html">Contact</a></li> 
+        </ul>
+      </div>
+
+      <div class="column">
+        <ul>
+          <li><a href="#">Search</a></li>
+          <li><a href="#">Deliveries & Refunds</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Terms Of Service</a></li>
+        </ul>
+      </div>
+
+
+      <div class="column">
+        <div class="email-subscription">
+        <p>Receive updates on our products and events in store</p>
+        <form>
+          <input type="email" placeholder="Email address">
+          <button type="submit">Subscribe</button>
+        </form>
+        </div>
+      </div>
+    </div>
+
+    <hr class="solid">
+    <p>© 2023 càphê</p>
+  </div>`
