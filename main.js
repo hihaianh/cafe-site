efe4f32eeaf8239f3a0eda18b46d1f5fa96c246d
Anh <97702902+hihaianh@users.noreply.github.com>
2023-07-21 22:29:36 +0100

    search container toggle
    search container is hidden and shown once triggered by the event of a click (on the magnifying glass (nav bar))

--- main.js
+++ main.js
@@ -15,17 +15,61 @@
       <li><a href="about.html">About Us</a></li>
       <li><a href="contact.html">Contact</a></li>
       <li>
-        <a href="#">
-          <i class="fa-light fa-magnifying-glass"></i>
+        <a href="#" id="searchIcon">
+          <i class="fa-solid fa-magnifying-glass" style="color: #000000;"></i>
         </a>
       </li>
       <li>
         <a href="#">
-          <i class="fa-sharp fa-light fa-cart-shopping" style="color: #000000;"></i>
+          <i class="fas fa-shopping-cart" style="color: #000000;"></i>
         </a>
       </li>
     </ul>
-  </nav>`
+  </nav>
+  <div id="searchContainer" class="peekaboo">
+    <div class="navbar-container">
+    <nav>
+    <div style="width: 100px">
+      <a href="index.html"><img src="logo.png" style="width: 100%; height: auto" /></a>
+    </div>
+    <ul>
+      <div class="dropdown">
+        <li><a href="#">Visit &#x2304;</a>
+          <div class="dropdown-content">
+              <a href="visit.html">Shoreditch</a>
+              <a href="menu.html">Menu</a>
+          </div>
+        </li>
+      </div>
+      <li><a href="shop.html">Shop</a></li>
+      <li><a href="about.html">About Us</a></li>
+      <li><a href="contact.html">Contact</a></li>
+      <li>
+        <a href="#" id="searchIcon">
+          <i class="fa-solid fa-magnifying-glass" style="color: #000000;"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-shopping-cart" style="color: #000000;"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+    </div>
+      <div class="search-content">
+        <div id="closeIcon">
+          <i class="fa-solid fa-xmark fa-2xl" style="color: #000000;"></i>
+        </div>
+        <div class="searchLine-container">
+          <input type="text" id="searchInput" placeholder="Search...">
+        </div>
+        <div class="search-click">
+          <i class="fa-solid fa-magnifying-glass fa-2xl" style="color: #000000;"></i>
+        </div>
+      </div>
+    </div>
+  `
 
     footer.innerHTML = `<div id="footer-container">
     <div class="columns">
@@ -47,11 +91,6 @@
           </ul>
         </div>
       </div>
-
-      <!-- <div class="column">
-        <p>99 Hackney Road, Shoreditch, London, E1 8TB</p>
-        <p>14 Brick Lane, London, E2 9DF</p>
-      </div> -->
 
       <div class="column">
         <ul>
@@ -86,3 +125,93 @@
     <hr class="solid">
     <p>© 2023 càphê</p>
   </div>`
+
+//add event listener for search icon
+let search = document.getElementById('searchIcon')
+console.log(search)
+search?.addEventListener('click', toggleSearch)
+//add event listener to close icon (in hidden 'search' container) to show underlying page and close search
+let close = document.getElementById('closeIcon')
+close.addEventListener('click', toggleSearch)
+//search icon once clicked will open up hidden container
+function toggleSearch() {
+  const searchContainer = document.getElementById('searchContainer');
+  const searchInput = document.getElementById('searchInput');
+  const grabElement = document.getElementById('testing');
+  searchContainer.classList.toggle('peekaboo');
+}
+
+//perform search based on user's input
+function performSearch() {
+  let searchQuery = searchInput.value.trim();
+  //search a list of items / fetch results from an API
+
+}
+
+
+
+// const cart = [];
+
+// //function to handle adding an item to cart
+// function addToCart(event) {
+//   const button = event.target;
+//   const productName = button.dataset.product;
+//   const productPrice = parseFloat(button.dataset.price);
+
+//   cart.push({name: productName, price: productPrice})
+
+//   displayCart();
+// }
+
+// //function to display shopping cart contents
+// function displayCart() {
+//   const cartDiv = document.getElementById('cart');
+//   cartDiv.innerHTML = '';
+
+//   //calculate total price of all items in cart
+//   let totalPrice = 0;
+
+//   cart.forEach(item => {
+//     const itemDiv = document.createElement('div');
+//     itemDiv.innerHTML = `${item.name} - $${item.price.toFixed(2)}`;
+//     cartDiv.appendChild(itemDiv)
+
+//     totalPrice += item.price
+//   })
+
+//   //display total price
+
+//   const totalDiv = document.createElement('div');
+//   totalDiv.innerHTML = `<strong>Total: $${totalPrice.toFixed(2)}</strong>`;
+//   cartDiv.appendChild(totalDiv);
+// }
+
+// //added event listeners to all add to cart buttons- it'll add to cart when you 'click' the button
+
+// const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+// addToCartButtons.forEach(button => {
+//   button.addEventListener('click', addToCart)
+// });
+
+
+
+
+//   //function to perform search
+// function performSearch() {
+//   //get the search term from the input field
+
+// }
+
+
+// //attach event Listener to search button
+
+// document.getElementById('search-icon')
+
+// document.getElementById('search-word').addEventListener('click', performSearch())
+
+// //Make search work by just pressing enter key
+// document.getElementById('searchInput').addEventListener('keypress', function(event) {
+//   if (event.key === 'Enter') {
+//     performSearch();
+//   }
+// })
