d47983444589b1d1abae19ea36a1c972cd59a81f
Anh <97702902+hihaianh@users.noreply.github.com>
2024-02-08 16:14:04 +0000

    login works
    register now doesn't work after fixing a few syntax errors. but login works.

---
+++ auth/login.php
@@ -0,0 +1,58 @@
+<?php
+
+$is_invalid = false;
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    $mysqli = require __DIR__ . "/database.php";
+
+    $sql = sprintf("SELECT * FROM user 
+                    WHERE email = '%s'", 
+                    $mysqli->real_escape_string($_POST['email'])
+    );
+
+    $result = $mysqli->query($sql);
+    $user = $result->fetch_assoc();
+
+    if($user) {
+        if(password_verify($_POST["password"], $user["password_hash"])) {
+            session_start();
+            session_regenerate_id();
+            $_SESSION["user_id"] = $user["id"];
+            header("Location: ../index.html");
+            exit;
+        } 
+    }
+    $is_invalid = true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Log In</h1>
+
+        <?php if($is_invalid): ?>
+            <em>Invalid login</em>
+        <?php endif; ?>
+
+    <form method='POST'>
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+
+        <button>Log In</button>
+
+    </form>
+<!-- testing with user acc@ email: hiannnguyen@gmail.com password: Kpopper12 -->
+</body>
+</html>
+
